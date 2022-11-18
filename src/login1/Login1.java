b5711453bdf0f798195e73d02b59d76d4de7971c
Sonu0103 <sonuhp2002@gmail.com>
2022-11-18 13:27:17 +0545

    login page started

---
+++ src/login1/Login1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login1;
+
+/**
+ *
+ * @author Acer
+ */
+public class Login1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login l=new login();
+//        l.setVisible()
+        // TODO code application logic here
+    }
+    
+}
